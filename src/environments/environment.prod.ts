830c0f7c9defb719988bd0ebf23bbbc2a7d5788e
Fabian Duran <ingfabianavellaneda@gmail.com>
2024-04-16 20:19:39 -0500

    Install gh-pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
-  production: true,
-  baseUrl: 'Pendiente prod'
+  production: false,
+  PUBLIC_KEY: '7ad4055bb3f4d0ba04c5a76ef43a2d0f',
+  PRIVATE_KEY: '336398964c4af71efc90cdf1109fafd94f546e65',
+  TS: '1',
+  HASH: '31c922fa279106c58dd401687ebd7ad7',
+  API_URL: 'https://gateway.marvel.com/v1/public/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,7 @@ export const environment = {
   PRIVATE_KEY: '336398964c4af71efc90cdf1109fafd94f546e65',
   TS: '1',
   HASH: '31c922fa279106c58dd401687ebd7ad7',
-  API_URL: 'http://gateway.marvel.com/v1/public/',
-  baseUrl: 'http://localhost:3000',
+  API_URL: 'https://gateway.marvel.com/v1/public/',
 };
 
 /*
